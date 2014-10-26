ba664e3fcbb217b51bd436da0c06dc1835f23c73
myfluxi <linflux@arcor.de>
2014-10-26 12:21:59 +0100

    msm: mdss: Cleanup some LGE specific bits

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -26,9 +26,6 @@
 
 #define DT_CMD_HDR 6
 
-static bool mdss_panel_flip_ud = false;
-static int mdss_panel_id = PANEL_QCOM;
-
 DEFINE_LED_TRIGGER(bl_led_trigger);
 
 #if defined(CONFIG_BACKLIGHT_LM3630)
@@ -202,29 +199,26 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 	pinfo = &(ctrl_pdata->panel_data.panel_info);
 
 	if (enable) {
-		if (mdss_panel_id == PANEL_LGE_JDI_ORISE_VIDEO ||
-			mdss_panel_id == PANEL_LGE_JDI_ORISE_CMD ||
-			mdss_panel_id == PANEL_LGE_JDI_NOVATEK_VIDEO ||
-			mdss_panel_id == PANEL_LGE_JDI_NOVATEK_CMD) {
-			if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
-				gpio_set_value((ctrl_pdata->disp_en_gpio), 1);
-			usleep(20 * 1000);
-			gpio_set_value((ctrl_pdata->rst_gpio), 1);
-			usleep(15 * 1000);
-			gpio_set_value((ctrl_pdata->rst_gpio), 0);
-			udelay(20);
-			gpio_set_value((ctrl_pdata->rst_gpio), 1);
-			usleep(10 * 1000);
-		} else {
-			gpio_set_value((ctrl_pdata->rst_gpio), 1);
-			msleep(20);
-			gpio_set_value((ctrl_pdata->rst_gpio), 0);
-			udelay(200);
-			gpio_set_value((ctrl_pdata->rst_gpio), 1);
-			msleep(20);
-			if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
-				gpio_set_value((ctrl_pdata->disp_en_gpio), 1);
-		}
+#ifdef CONFIG_MACH_LGE
+		if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
+			gpio_set_value((ctrl_pdata->disp_en_gpio), 1);
+		usleep(20 * 1000);
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
+		usleep(15 * 1000);
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		udelay(20);
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
+		usleep(10 * 1000);
+#else
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
+		msleep(20);
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		udelay(200);
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
+		msleep(20);
+		if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
+			gpio_set_value((ctrl_pdata->disp_en_gpio), 1);
+#endif
 		if (gpio_is_valid(ctrl_pdata->mode_gpio)) {
 			if (pinfo->mode_gpio_state == MODE_GPIO_HIGH)
 				gpio_set_value((ctrl_pdata->mode_gpio), 1);
@@ -238,19 +232,16 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 			pr_debug("%s: Reset panel done\n", __func__);
 		}
 	} else {
-		if (mdss_panel_id == PANEL_LGE_JDI_ORISE_VIDEO ||
-			mdss_panel_id == PANEL_LGE_JDI_ORISE_CMD ||
-			mdss_panel_id == PANEL_LGE_JDI_NOVATEK_VIDEO ||
-			mdss_panel_id == PANEL_LGE_JDI_NOVATEK_CMD) {
-			if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
-				gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
-			usleep(20 * 1000);
-			gpio_set_value((ctrl_pdata->rst_gpio), 0);
-		} else {
-			gpio_set_value((ctrl_pdata->rst_gpio), 0);
-			if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
-				gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
-		}
+#ifdef CONFIG_MACH_LGE
+		if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
+			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
+		usleep(20 * 1000);
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+#else
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
+			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
+#endif
 	}
 }
 
@@ -473,16 +464,6 @@ static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 	return 0;
 }
 
-int mdss_dsi_panel_id(void)
-{
-	return mdss_panel_id;
-}
-
-bool mdss_dsi_panel_flip_ud(void)
-{
-	return mdss_panel_flip_ud;
-}
-
 static int mdss_panel_dt_get_dst_fmt(u32 bpp, char mipi_mode, u32 pixel_packing,
 				char *dst_format)
 {
@@ -644,15 +625,6 @@ static int mdss_panel_parse_dt(struct device_node *np,
 	const char *data;
 	static const char *pdest;
 	struct mdss_panel_info *pinfo = &(ctrl_pdata->panel_data.panel_info);
-
-	rc = of_property_read_u32(np, "qcom,mdss-pan-id", &tmp);
-	if (!rc)
-		mdss_panel_id = tmp;
-	pr_info("%s: Panel ID = %d\n", __func__, mdss_panel_id);
-
-	mdss_panel_flip_ud = of_property_read_bool(np, "qcom,mdss-pan-flip-ud");
-	if (mdss_panel_flip_ud)
-		pr_info("%s: Panel FLIP UD\n", __func__);
 
 	rc = of_property_read_u32(np, "qcom,mdss-dsi-panel-width", &tmp);
 	if (rc) {
